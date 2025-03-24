c602fa13cdfed0f353a559a44769acefe7b7b707
JeonBeomGu-S <jb98967@gmail.com>
2025-03-23 22:45:20 -0400

    Initialize project

---
+++ src/main/java/com/bam/lemmecook/LemmecookApplication.java
@@ -0,0 +1,13 @@
+package com.bam.lemmecook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemmecookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemmecookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bam/lemmecook/LemmecookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bam.lemmecook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemmecookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
